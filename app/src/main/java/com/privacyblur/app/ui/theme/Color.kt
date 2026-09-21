1264db908fd36513b484ee605e69e0fa7e6a4227
dheerajguptame-sys <dheerajgupta.me@gmail.com>
2026-09-21 17:24:42 +0530

    Restore app/src/main/java/com/privacyblur/app/ui/theme/Color.kt

---
+++ app/src/main/java/com/privacyblur/app/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.privacyblur.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Slate900 = Color(0xFF0F172A)
+val Slate800 = Color(0xFF1E293B)
+val Slate100 = Color(0xFFF1F5F9)
+val Sky500 = Color(0xFF0EA5E9)
+val Sky600 = Color(0xFF0284C7)
+val Emerald500 = Color(0xFF10B981)
+val Amber500 = Color(0xFFF59E0B)
